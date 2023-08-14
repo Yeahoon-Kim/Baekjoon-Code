7357ab83e94e31ca58550defff926212482cbe58
Yeahoon Kim <66424984+Yeahoon-Kim@users.noreply.github.com>
2023-08-14 18:13:57 +0900

    [Silver I] Title: 정수 삼각형, Time: 8 ms, Memory: 3084 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
@@ -1,48 +1,54 @@
 #include <bits/stdc++.h>
 
+#define ll long long int
+#define ull unsigned long long int
+#define lll __int128_t
+#define ld long double
+#define INF (int)1e9
+#define INFLL (long long)1e18
+#define pii pair<int, int>
+#define pll pair<long long int, long long int>
+#define MOD (INF + 7)
+
 using namespace std;
 
-void init(void);
+void init();
 
-#define INF 1e9
-#define INFLL 4e18
-
-int pyramid[501][501];
-
-int main(void) {
+int main() {
     init();
 
-    int pyramidSize, i, j, maxSum = 0;
+    int pyramidSize, i, j;
+    vector<vector<int>> pyramid;
+    int maxValue;
 
     cin >> pyramidSize;
-    for (i = 0; i < pyramidSize; i++) {
-        for(j = 0; j <= i; j++) cin >> pyramid[i][j];
-    }
+    pyramid.resize(pyramidSize, vector<int>(pyramidSize));
 
-    for(i = 1; i < pyramidSize; i++) {
+    for(i = 0; i < pyramidSize; i++) {
         for(j = 0; j <= i; j++) {
+            cin >> pyramid[i][j];
+
+            if(i == 0) continue;
             if(j == 0) pyramid[i][j] += pyramid[i - 1][j];
             else if(j == i) pyramid[i][j] += pyramid[i - 1][j - 1];
             else pyramid[i][j] += max(pyramid[i - 1][j], pyramid[i - 1][j - 1]);
         }
     }
 
-    for(i = 0; i < pyramidSize; i++) {
-        maxSum = max(maxSum, pyramid[pyramidSize - 1][i]);
-    }
+    maxValue = 0;
+    for(j = 0; j < pyramidSize; j++) maxValue = max(maxValue, pyramid[pyramidSize - 1][j]);
 
-    cout << maxSum;
+    cout << maxValue;
 
     return 0;
 }
 
-void init(void) {
+void init() {
 #ifndef ONLINE_JUDGE
-    freopen("input.txt", "rb", stdin);
-    freopen("output.txt", "wb", stdout);
+    freopen("input.txt", "rt+", stdin);
+    freopen("output.txt", "wt+", stdout);
 #endif
-
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
 }
