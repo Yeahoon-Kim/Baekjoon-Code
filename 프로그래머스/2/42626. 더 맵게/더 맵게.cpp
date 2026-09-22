71c037bcfcb33aacb7d01820d0a1b8b386e3cece
Yeahoon Kim <66424984+Yeahoon-Kim@users.noreply.github.com>
2026-09-22 16:07:50 +0900

    [level 2] Title: 더 맵게, Time: 191.16 ms, Memory: 41.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int solution(vector<int> scoville, int K) {
+    int answer = 0;
+    priority_queue<int, vector<int>, greater<int>> pq(scoville.begin(), scoville.end());
+    
+    while(pq.size() >= 2 && pq.top() < K) {
+        int a = pq.top(); pq.pop();
+        int b = pq.top(); pq.pop();
+        
+        pq.push(a+2*b);
+        answer++;
+    }
+    
+    
+    return (pq.top() >= K ? answer : -1);
+}
