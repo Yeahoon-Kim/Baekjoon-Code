79a8e7bfc5e0c3d4a47e7ee7c8463ac1bf883b45
Yeahoon Kim <66424984+Yeahoon-Kim@users.noreply.github.com>
2026-09-24 00:46:23 +0900

    [level 3] Title: 단속카메라, Time: 0.37 ms, Memory: 4.63 MB -BaekjoonHub

---
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<vector<int>> routes) {
+    int answer = 0, prev_cam = -1e9;
+    
+    sort(routes.begin(), routes.end(), [](const vector<int>& a, const vector<int>& b) {
+        return a[1] < b[1];
+    });
+    
+    for(const auto& route : routes) {
+        if(route[0] > prev_cam) {
+            prev_cam = route[1];
+            answer++;
+        }
+    }
+    
+    return answer;
+}
